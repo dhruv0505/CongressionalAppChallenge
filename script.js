05bd8bad98df9926e13f4173773cca4c3f074ef0
Aisha Lalchandani <dlalchandani05@gmail.com>
2022-10-11 17:02:05 -0700

    login + miscellanious fixes

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-function openProfilePage(userID){
+if ((typeof localStorage.getItem("userID") !== "undefined" && !(typeof localStorage.getItem("userID") === "string" && localStorage.getItem("userID")=="undefined")) && window.location.href.slice(-10)=="login.html"){
+    window.location.href = "home.html";
+}
+
+if ((typeof localStorage.getItem("userID") === "undefined" || (typeof localStorage.getItem("userID") === "string" && localStorage.getItem("userID")=="undefined"))&& window.location.href.slice(-10)!="login.html"){
+    window.location.href = "login.html";
+}
+
+function openProfile(){
+    window.location.href = "profilePage.html";
 }
 function openGroupPage(groupID){
+}
+function login(){
+    localStorage.setItem("userID", document.getElementById("usernameLoginInput").value);
+    window.location.href = "home.html";
+}
+function logout(){
+    localStorage.setItem("userID", "undefined");
+    window.location.href = "login.html";
 }
